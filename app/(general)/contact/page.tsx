f4a84e7b9cd5413d9f2c5633160f8fc30c2879ca
Jose Chale <chalejosejuan@gmail.com>
2024-06-22 11:59:26 -0600

    Initial commit

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+ title: 'Contact Page',
+ description: 'Esta es la pagina de contactos de la empresa',
+};
+export default function ContactPage(){
+    return(
+        <>
+
+        <span className="text-7xl">Contact Page </span>
+        </>
+    )
+}
---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+ title: 'Pricing Page',
+ description: 'Esta es la pagina de precios de la empresa',
+};
+export default function PricingPage(){
+    return(
+        <>
+
+        <span className="text-7xl">Pricing Page    </span>
+
+        </>
+    )
+}
